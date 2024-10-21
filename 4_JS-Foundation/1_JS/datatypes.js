e6b1957b800785d09f27ba4bcc1fb1cf6f1c7089
Anshul <andrewanshul798@gmail.com>
2024-10-21 12:12:24 +0530

    js-basics

---
+++ 4_JS-Foundation/1_JS/datatypes.js
@@ -0,0 +1,21 @@
+console.log("hello");
+/* 
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+Symbol
+*/
+
+let score=100;
+let name = "Hmloo"
+let isLoggedin=false
+
+//object
+let teaTypes=["lemon tea","orange tea", "oolong tea"]
+let user = {firstname: "Ansh", lastname: "Gupta"}
