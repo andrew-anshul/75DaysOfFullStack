3e20b164f7282cd4551eceefb81d2b34f204240a
Anshul <andrewanshul798@gmail.com>
2024-10-26 21:19:43 +0530

    value undefined error resolved

--- Practice/todo-localStorage-tailwind/src/script.js
+++ Practice/todo-localStorage-tailwind/src/script.js
@@ -2,10 +2,12 @@ const todoInput = document.getElementById("todo-input");
 const addTaskButton = document.getElementById("add-task-btn");
 const todoList = document.getElementById("todo-list");
 
-let tasks = localStorage.getItem('tasks') || [];
-tasks.forEach(task => renderTask(task))
+let tasks =  [];
+
 addTaskButton.addEventListener('click', () => {
   const taskTest = todoInput.value.trim()
+
+  
   if (taskTest === "") return;
 
   const newTask = {
@@ -19,11 +21,7 @@ addTaskButton.addEventListener('click', () => {
   console.log(tasks);
 
 });
-function renderTask(task) {
-const li = document.createElement('li')
-li.setAttribute('data-id',task.id)
-li.innerHTML = `<span${task.text}>`
-}
+
 function saveTasks() {
   localStorage.setItem('tasks', JSON.stringify(tasks))
 }
